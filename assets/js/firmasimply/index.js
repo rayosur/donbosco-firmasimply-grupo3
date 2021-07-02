acd318112e4149bfb1c89568e4765c98f5f042e3
unknown <robles>
2021-07-02 10:52:42 +0200

    viernes

--- assets/js/firmasimply/index.js
+++ assets/js/firmasimply/index.js
@@ -1,17 +1,51 @@
 import Auth from './Modules/Auth/Auth.js';
 import Asistencia from './Modules/Asistencia.js';
-import Tarea from './Modules/Tarea.js';
-import Pildora from './Modules/Pildora.js';
 
 
+
+
+// Tu Código
+const list = document.getElementById('lista_firmas');
 async function getListadoFirmas() {
-    let respuesta = await Asistencia.getlistadoFirmas();
-    //console.log(respuesta);
-    // Tu Código
+  console.log('hola');
+  let res = await Asistencia.getListadoFirmas();
+  for (var i = 0; i < res.length; i++) {
+    console.log(res[i].name);
+    list.innerHTML += `<tr>
+        <td>
+          ${res[i].nombre}
+        </td>
+        <td>
+          ${res[i].apellido}
+        </td>
+      </tr>`;
+  }
+
 }
 
+getListadoFirmas();
+
+
 // Funcionalidad de Firmar
+let botonFirmaEntrada = document.getElementById("buttonFirmar")
+botonFirmaEntrada.addEventListener("click", async (e) => {
+  alert("has firmado entrada")
+
+
+  await Firma.crearEntrada(entrada);
+  window.location.reload();
+});
+
+let botonFirmaSalida = document.getElementById("buttonFirmar")
+botonFirmaSalida.addEventListener("click", async (e) => {
+  alert("has firmado salida")
+
+  await Firma.crearSalida(salida);
+  window.location.reload();
+});
+
+
 
 // Funcionalidad mostrar Listado Tareas
 
-// Funcionalidad mostrar Listado Píldoras
+// Funcionalidad mostrar Listado Píldorasconst input = document.querySelector("input");
